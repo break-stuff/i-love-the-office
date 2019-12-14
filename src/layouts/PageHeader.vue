cadead5093b531772a639fdaa23840a271b32bbb
break-stuff <burton@breakstuff.com>
2019-12-14 11:02:41 -0500

    Initial code layout

---
+++ src/layouts/PageHeader.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNavDropdown"
+            aria-controls="navbarNavDropdown"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+        >
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/">Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" to="/about">About</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" title="Favorites" to="/favorites"><span class="sr-only">Favorites</span><i class="fas fa-heart"></i></router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {};
+</script>
