68801b07c4063b88978a91210a2ff1b8567efcee
break-stuff <burton@breakstuff.com>
2019-12-29 00:39:43 -0500

    Add Bootstrap-Vue to project

--- src/main.js
+++ src/main.js
@@ -1,15 +1,17 @@
-import 'bootstrap';
-import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import BootstrapVue from 'bootstrap-vue';
 
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
