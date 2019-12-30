c5106d4b6a8b3a3f4629bf0478d2a53af8878cf3
break-stuff <burton@breakstuff.com>
2019-12-30 16:58:36 -0500

    Added sample data

---
+++ src/data/quotes.js
@@ -0,0 +1,162 @@
+export default [
+    {
+        quote: "A real man swallows his vomit when a lady is present.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "Abraham Lincoln once said that, 'If you're a racist, I will attack you with the North.' And those are the principles that I carry with me in the workplace.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked, but it's not like this compulsive need to be liked, like my need to be praised.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Dwight you ignorant sl**.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Growing up I performed my own circumcision.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "Guess what, I have flaws. What are they? Oh I don't know. I sing in the shower. Sometimes I spend too much time volunteering. Occasionally I'll hit somebody with my car. So sue me.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "I am about to do something very bold in this job that I've never done before... try.",
+        character: "Jim Halpert"
+    },
+    {
+        quote: "I am running away from my responsibilities. And it feels good.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "I don’t have a lot of experience with vampires, but I have hunted werewolves. I shot one once, but by the time I got to it, it had turned back into my neighbor’s dog.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "I feel like all my kids grew up and then they married each other. It's every parents' dream.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "I have been trying to get on jury duty every year since I was 18 years old. To get and go sit in an air-conditioned room, downtown, judging people, while my lunch was paid for. That is the life.",
+        character: "Stanley Hudson"
+    },
+    {
+        quote: "I talk a lot, so I’ve learned to tune myself out.",
+        character: "Kelly Kapoor"
+    },
+    {
+        quote: "I've never been a kiss up. It's just not how I operate. I mean I've always subscribed to the idea that if you really want to impress your boss, you go in there and you do mediocre work, half-heartedly.",
+        character: "Jim Halpert"
+    },
+    {
+        quote: "I’m an early bird and I’m a night owl. So I’m wise and I have worms.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "I’m not superstitious, but I am a little stitious.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "I’ve been involved in a number of cults, both as a leader and a follower. You have more fun as a follower. but you make more money as a leader.",
+        character: "Creed Bratton"
+    },
+    {
+        quote: "Identity theft is not a joke, Jim! Millions of families suffer every year.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "If I don’t have some cake soon, I might die.",
+        character: "Stanley Hudson"
+    },
+    {
+        quote: "If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "It's never too early for ice cream, Jim. But we didn't have any ice cream, so this is mayonnaise and black olives. It's comfort food, alright?",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Maybe we weren't right together, but, it's weird. I'd rather she be alone than with somebody. Is that love?",
+        character: "Ryan Howard"
+    },
+    {
+        quote: "Michael always says, 'K-I-S-S, keep it simple, stupid.' Great advice, hurts my feelings every time.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "Michael is like Mozart, and I’m like Butch Cassidy. You mess with Mozart and you’re gonna get bullet in your head, courtesy of Butch Cassidy.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "My perfect Valentine's day? I'm at home, three cell phones in front of me, fielding desperate calls from people who want to buy one of the fifty restaurant reservations I made over six months ago.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "No, Rose, they are not breathing. And they have no arms or legs … Where are they? You know what? If we come across somebody with no arms or legs, do we bother resuscitating them? I mean, what quality of life do we have there?",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Nobody steals from Creed Bratton and gets away with it. The last person to do this disappeared. His name: Creed Bratton.",
+        character: "Creed Bratton"
+    },
+    {
+        quote: "Nothing stresses me out. Except having to seek the approval of my inferiors.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "Powerpoints are the peacocks of the business world; all show, no meat.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "Reverse psychology is an awesome tool. I don't know if you guys know about it, but, basically, you make someone think the opposite of what you believe. And that tricks them into doing something stupid. Works like a charm.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Sometimes I feel like everyone I work with is an idiot. And by sometimes I mean all times. All the times. Every of the times!",
+        character: "Kevin Malone"
+    },
+    {
+        quote: "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Sometimes the clothes at Gap Kids are too flashy, so I’m forced to go to the American Girl store and order clothes for large colonial dolls.",
+        character: "Angela Martin"
+    },
+    {
+        quote: "The eyes are the groin of the face.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "The worst thing about prison was the dementors.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Today, smoking is going to save lives.",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "Who says exactly what they’re thinking? What kind of a game is that?",
+        character: "Kelly Kapoor"
+    },
+    {
+        quote: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+        character: "Michael Scott"
+    },
+    {
+        quote: "Yes. I have decided to shun Andy Bernard for the next three years. Which I'm looking for",
+        character: "Dwight Schrute"
+    },
+    {
+        quote: "You cheated on me? When I specifically asked you not to?",
+        character: "Michael Scott"
+    },
+    {
+        quote: "You're a perfectly fine toilet... I'm just an extraordinary piece of crap.",
+        character: "Dwight Schrute"
+    }
+];
