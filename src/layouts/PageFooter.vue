a47777ecc34e21eb3ebdcf2b7bc11a307eca4da8
break-stuff <burton@breakstuff.com>
2019-12-31 13:43:06 -0500

    added links to contact info

--- src/layouts/PageFooter.vue
+++ src/layouts/PageFooter.vue
@@ -1,7 +1,7 @@
 <template>
     <footer>
         <b-navbar type="dark" variant="dark">
-            <b-navbar-brand href="#" class="text-center">Created by Burton Smith @stuffbreaker</b-navbar-brand>
+            <span class="text-white">Created by <a class="text-white font-weight-bold" href="https://www.linkedin.com/in/burton-smith-48132a34/">Burton Smith</a> - <a class="text-white font-weight-bold" href="https://twitter.com/stuffbreaker">@stuffbreaker</a></span>
         </b-navbar>
     </footer>
 </template>
