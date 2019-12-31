fae1176b030b22f8681b3c4e9856564d3cecd0f9
break-stuff <burton@breakstuff.com>
2019-12-31 13:42:54 -0500

    removed default component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
     <div class="home">
-        <img alt="Vue logo" src="../assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <div class="text-center">
+            <img
+                class="mb-5 w-90"
+                alt="image of the office cast"
+                src="../assets/images/the_office_hero.png" />
+            <h1 class="display-5">Welcome to I Love the Office - Vue!</h1>
+        </div>
+
+        <p>
+            This project is designed to get users up and running quickly with
+            <a href="https://vuejs.org/">Vue.js</a>, the JavaScript framework. To get the workshop content, go to
+            <a href="https://github.com/break-stuff/i-love-the-office">https://github.com/break-stuff/i-love-the-office</a>.
+        </p>
+
+        <p>This workshop will take you through a series of challenges that are designed to get you familiar with the capabilities of Vue.js and equip you with the knowledge you need to start building your own Vue applications.</p>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "../components/HelloWorld.vue";
-
-export default {
-    name: "home",
-    components: {
-        HelloWorld
-    }
-};
+    export default {
+        name: "home",
+        components: {}
+    };
 </script>
