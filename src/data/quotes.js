94bf7800a8491cfba60d9e368f848a021d832576
break-stuff <burton@breakstuff.com>
2020-01-01 01:29:36 -0500

    Update default data

--- src/data/quotes.js
+++ src/data/quotes.js
@@ -1,202 +1,242 @@
-export default [[
+export default [
     {
-        quote: "A real man swallows his vomit when a lady is present.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 1,
+        "quote": "A real man swallows his vomit when a lady is present.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "Abraham Lincoln once said that, 'If you're a racist, I will attack you with the North.' And those are the principles that I carry with me in the workplace.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 2,
+        "quote": "Abraham Lincoln once said that, 'If you're a racist, I will attack you with the North.' And those are the principles that I carry with me in the workplace.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked, but it's not like this compulsive need to be liked, like my need to be praised.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 3,
+        "quote": "Do I need to be liked? Absolutely not. I like to be liked. I enjoy being liked. I have to be liked, but it's not like this compulsive need to be liked, like my need to be praised.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Dwight you ignorant sl**.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 4,
+        "quote": "Dwight you ignorant sl**.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Growing up I performed my own circumcision.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 5,
+        "quote": "Growing up I performed my own circumcision.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "Guess what, I have flaws. What are they? Oh I don't know. I sing in the shower. Sometimes I spend too much time volunteering. Occasionally I'll hit somebody with my car. So sue me.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 6,
+        "quote": "Guess what, I have flaws. What are they? Oh I don't know. I sing in the shower. Sometimes I spend too much time volunteering. Occasionally I'll hit somebody with my car. So sue me.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "I am about to do something very bold in this job that I've never done before... try.",
-        character: "Jim Halpert",
-        isFavorite: false
+        "id": 7,
+        "quote": "I am about to do something very bold in this job that I've never done before... try.",
+        "characterId": 17697,
+        "isFavorite": false
     },
     {
-        quote: "I am running away from my responsibilities. And it feels good.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 8,
+        "quote": "I am running away from my responsibilities. And it feels good.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "I don’t have a lot of experience with vampires, but I have hunted werewolves. I shot one once, but by the time I got to it, it had turned back into my neighbor’s dog.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 9,
+        "quote": "I don’t have a lot of experience with vampires, but I have hunted werewolves. I shot one once, but by the time I got to it, it had turned back into my neighbor’s dog.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "I feel like all my kids grew up and then they married each other. It's every parents' dream.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 10,
+        "quote": "I feel like all my kids grew up and then they married each other. It's every parents' dream.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "I have been trying to get on jury duty every year since I was 18 years old. To get and go sit in an air-conditioned room, downtown, judging people, while my lunch was paid for. That is the life.",
-        character: "Stanley Hudson",
-        isFavorite: false
+        "id": 11,
+        "quote": "I have been trying to get on jury duty every year since I was 18 years old. To get and go sit in an air-conditioned room, downtown, judging people, while my lunch was paid for. That is the life.",
+        "characterId": 1230842,
+        "isFavorite": false
     },
     {
-        quote: "I talk a lot, so I’ve learned to tune myself out.",
-        character: "Kelly Kapoor",
-        isFavorite: false
+        "id": 12,
+        "quote": "I talk a lot, so I’ve learned to tune myself out.",
+        "characterId": 304283,
+        "isFavorite": false
     },
     {
-        quote: "I've never been a kiss up. It's just not how I operate. I mean I've always subscribed to the idea that if you really want to impress your boss, you go in there and you do mediocre work, half-heartedly.",
-        character: "Jim Halpert",
-        isFavorite: false
+        "id": 13,
+        "quote": "I've never been a kiss up. It's just not how I operate. I mean I've always subscribed to the idea that if you really want to impress your boss, you go in there and you do mediocre work, half-heartedly.",
+        "characterId": 17697,
+        "isFavorite": false
     },
     {
-        quote: "I’m an early bird and I’m a night owl. So I’m wise and I have worms.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 14,
+        "quote": "I’m an early bird and I’m a night owl. So I’m wise and I have worms.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "I’m not superstitious, but I am a little stitious.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 15,
+        "quote": "I’m not superstitious, but I am a little stitious.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "I’ve been involved in a number of cults, both as a leader and a follower. You have more fun as a follower. but you make more money as a leader.",
-        character: "Creed Bratton",
-        isFavorite: false
+        "id": 16,
+        "quote": "I’ve been involved in a number of cults, both as a leader and a follower. You have more fun as a follower. but you make more money as a leader.",
+        "characterId": 85177,
+        "isFavorite": false
     },
     {
-        quote: "Identity theft is not a joke, Jim! Millions of families suffer every year.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 17,
+        "quote": "Identity theft is not a joke, Jim! Millions of families suffer every year.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "If I don’t have some cake soon, I might die.",
-        character: "Stanley Hudson",
-        isFavorite: false
+        "id": 18,
+        "quote": "If I don’t have some cake soon, I might die.",
+        "characterId": 1230842,
+        "isFavorite": false
     },
     {
-        quote: "If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 19,
+        "quote": "If I were buying my coffin, I would get one with thicker walls so you couldn’t hear the other dead people.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "It's never too early for ice cream, Jim. But we didn't have any ice cream, so this is mayonnaise and black olives. It's comfort food, alright?",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 20,
+        "quote": "It's never too early for ice cream, Jim. But we didn't have any ice cream, so this is mayonnaise and black olives. It's comfort food, alright?",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Maybe we weren't right together, but, it's weird. I'd rather she be alone than with somebody. Is that love?",
-        character: "Ryan Howard",
-        isFavorite: false
+        "id": 21,
+        "quote": "Maybe we weren't right together, but, it's weird. I'd rather she be alone than with somebody. Is that love?",
+        "characterId": 304284,
+        "isFavorite": false
     },
     {
-        quote: "Michael always says, 'K-I-S-S, keep it simple, stupid.' Great advice, hurts my feelings every time.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 22,
+        "quote": "Michael always says, 'K-I-S-S, keep it simple, stupid.' Great advice, hurts my feelings every time.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "Michael is like Mozart, and I’m like Butch Cassidy. You mess with Mozart and you’re gonna get bullet in your head, courtesy of Butch Cassidy.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 23,
+        "quote": "Michael is like Mozart, and I’m like Butch Cassidy. You mess with Mozart and you’re gonna get bullet in your head, courtesy of Butch Cassidy.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "My perfect Valentine's day? I'm at home, three cell phones in front of me, fielding desperate calls from people who want to buy one of the fifty restaurant reservations I made over six months ago.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 24,
+        "quote": "My perfect Valentine's day? I'm at home, three cell phones in front of me, fielding desperate calls from people who want to buy one of the fifty restaurant reservations I made over six months ago.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "No, Rose, they are not breathing. And they have no arms or legs … Where are they? You know what? If we come across somebody with no arms or legs, do we bother resuscitating them? I mean, what quality of life do we have there?",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 25,
+        "quote": "No, Rose, they are not breathing. And they have no arms or legs … Where are they? You know what? If we come across somebody with no arms or legs, do we bother resuscitating them? I mean, what quality of life do we have there?",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Nobody steals from Creed Bratton and gets away with it. The last person to do this disappeared. His name: Creed Bratton.",
-        character: "Creed Bratton",
-        isFavorite: false
+        "id": 26,
+        "quote": "Nobody steals from Creed Bratton and gets away with it. The last person to do this disappeared. His name: Creed Bratton.",
+        "characterId": 85177,
+        "isFavorite": false
     },
     {
-        quote: "Nothing stresses me out. Except having to seek the approval of my inferiors.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 27,
+        "quote": "Nothing stresses me out. Except having to seek the approval of my inferiors.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "Powerpoints are the peacocks of the business world; all show, no meat.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 28,
+        "quote": "Powerpoints are the peacocks of the business world; all show, no meat.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "Reverse psychology is an awesome tool. I don't know if you guys know about it, but, basically, you make someone think the opposite of what you believe. And that tricks them into doing something stupid. Works like a charm.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 29,
+        "quote": "Reverse psychology is an awesome tool. I don't know if you guys know about it, but, basically, you make someone think the opposite of what you believe. And that tricks them into doing something stupid. Works like a charm.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Sometimes I feel like everyone I work with is an idiot. And by sometimes I mean all times. All the times. Every of the times!",
-        character: "Kevin Malone",
-        isFavorite: false
+        "id": 30,
+        "quote": "Sometimes I feel like everyone I work with is an idiot. And by sometimes I mean all times. All the times. Every of the times!",
+        "characterId": 94622,
+        "isFavorite": false
     },
     {
-        quote: "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 31,
+        "quote": "Sometimes I’ll start a sentence and I don’t even know where it’s going. I just hope I find it along the way.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Sometimes the clothes at Gap Kids are too flashy, so I’m forced to go to the American Girl store and order clothes for large colonial dolls.",
-        character: "Angela Martin",
-        isFavorite: false
+        "id": 32,
+        "quote": "Sometimes the clothes at Gap Kids are too flashy, so I’m forced to go to the American Girl store and order clothes for large colonial dolls.",
+        "characterId": 113867,
+        "isFavorite": false
     },
     {
-        quote: "The eyes are the groin of the face.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 33,
+        "quote": "The eyes are the groin of the face.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "The worst thing about prison was the dementors.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 34,
+        "quote": "The worst thing about prison was the dementors.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Today, smoking is going to save lives.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 35,
+        "quote": "Today, smoking is going to save lives.",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "Who says exactly what they’re thinking? What kind of a game is that?",
-        character: "Kelly Kapoor",
-        isFavorite: false
+        "id": 36,
+        "quote": "Who says exactly what they’re thinking? What kind of a game is that?",
+        "characterId": 304283,
+        "isFavorite": false
     },
     {
-        quote: "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 37,
+        "quote": "Would I rather be feared or loved? Easy. Both. I want people to be afraid of how much they love me.",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "Yes. I have decided to shun Andy Bernard for the next three years. Which I'm looking for",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 38,
+        "quote": "Yes. I have decided to shun Andy Bernard for the next three years. Which I'm looking for",
+        "characterId": 11678,
+        "isFavorite": false
     },
     {
-        quote: "You cheated on me? When I specifically asked you not to?",
-        character: "Michael Scott",
-        isFavorite: false
+        "id": 39,
+        "quote": "You cheated on me? When I specifically asked you not to?",
+        "characterId": 4495,
+        "isFavorite": false
     },
     {
-        quote: "You're a perfectly fine toilet... I'm just an extraordinary piece of crap.",
-        character: "Dwight Schrute",
-        isFavorite: false
+        "id": 40,
+        "quote": "You're a perfectly fine toilet... I'm just an extraordinary piece of crap.",
+        "characterId": 11678,
+        "isFavorite": false
     }
 ];
