dfed80e56670cb1554df95f55d5cbaf1196423dd
break-stuff <burton@breakstuff.com>
2020-01-05 16:46:51 -0500

    Updated styling and copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,54 +6,66 @@
                 alt="image of the office cast"
                 src="../assets/images/the_office_hero.jpg"
             />
-            <h1 class="display-5">Welcome to I Love the Office - Vue!</h1>
+            <h1 class="display-5">Welcome to I Love the Office!</h1>
+            <h2>(Vue.js Addtion)</h2>
         </div>
-
+        <hr />
         <p>
             This project is designed to get users up and running quickly with
             <a
+                target="_blank"
                 href="https://vuejs.org/"
             >Vue.js</a> (a JavaScript framework). To get the workshop content, go to
             <a
+                target="_blank"
                 href="https://github.com/break-stuff/i-love-the-office"
             >https://github.com/break-stuff/i-love-the-office</a>.
         </p>
 
-        <p>This workshop will take you through a series of challenges that are designed to get you familiar with the capabilities of Vue.js and equip you with the knowledge you need to start building your own Vue applications.</p>
+        <p>This workshop will take you through a series of challenges that are designed to get you familiar with the capabilities of Vue.js and equip you with the knowledge you need to start building your own Vue.js applications.</p>
+
+        <h2 class="mt-5">Table of Contents</h2>
 
         <ul class="list-group">
             <li class="list-group-item">
                 <a
+                    target="_blank"
                     href="https://github.com/break-stuff/i-love-the-office/tree/master/challenges/challenge_1"
                 >Challenge 1 - Display Quotes</a>
             </li>
             <li class="list-group-item">
                 <a
+                    target="_blank"
                     href="https://github.com/break-stuff/i-love-the-office/tree/master/challenges/challenge_2"
                 >Challenge 2 - Favorites</a>
             </li>
             <li class="list-group-item">
                 <a
+                    target="_blank"
                     href="https://github.com/break-stuff/i-love-the-office/tree/master/challenges/challenge_3"
                 >Challenge 3 - Filter List</a>
             </li>
             <li class="list-group-item">
                 <a
+                    target="_blank"
                     href="https://github.com/break-stuff/i-love-the-office/tree/master/challenges/challenge_4"
                 >Challenge 4 - Components</a>
             </li>
             <li class="list-group-item">
                 <a
+                    target="_blank"
                     href="https://github.com/break-stuff/i-love-the-office/tree/master/challenges/challenge_5"
                 >Challenge 5 - Advanced Components</a>
             </li>
             <li class="list-group-item">
                 <a
+                    target="_blank"
                     href="https://github.com/break-stuff/i-love-the-office/tree/master/challenges/challenge_6"
                 >Challenge 6 - Custom Events</a>
             </li>
             <li class="list-group-item">
                 <a
+                    target="_blank"
                     href="https://github.com/break-stuff/i-love-the-office/tree/master/challenges/challenge_7"
                 >Challenge 7 - Adding a Quote</a>
             </li>
