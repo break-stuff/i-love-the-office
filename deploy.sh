e2da27921e009d17cb4feb9db3904f392ce5604d
break-stuff <burton@breakstuff.com>
2020-01-05 20:01:02 -0500

    Added deployment config

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:break-stuff/i-love-the-office.git master:gh-pages
+git push -f https://github.com/break-stuff/i-love-the-office.git master:gh-pages
 
 cd -
